4520a09aefb59b9cd32fd927cd68296c082ec315
MaksimMyson <maksimmyson33@gmail.com>
2024-02-27 20:24:30 +0200

    exe3

--- CSharp4_27_02/Class1.cs
+++ CSharp4_27_02/Class1.cs
@@ -1,7 +1,88 @@
 ﻿namespace CSharp4_27_02
 {
-    public class Class1
+    public class City
     {
+    private string name;
+    private int population;
 
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
     }
+
+    public int Population
+    {
+        get { return population; }
+        set { population = value; }
+    }
+
+    public City(string name, int population)
+    {
+        Name = name;
+        Population = population;
+    }
+
+    
+    public static City operator +(City city, int increase)
+    {
+        city.Population += increase;
+        return city;
+    }
+
+    
+    public static City operator -(City city, int decrease)
+    {
+        if (city.Population - decrease < 0)
+            throw new InvalidOperationException("Неможливо відняти від кількості жителів, що вже є нульовою або від'ємною.");
+
+        city.Population -= decrease;
+        return city;
+    }
+
+    
+    public static bool operator ==(City city1, City city2)
+    {
+        if (ReferenceEquals(city1, null))
+            return ReferenceEquals(city2, null);
+        if (ReferenceEquals(city2, null))
+            return false;
+
+        return city1.Population == city2.Population;
+    }
+
+    
+    public static bool operator !=(City city1, City city2)
+    {
+        return !(city1 == city2);
+    }
+
+    
+    public static bool operator >(City city1, City city2)
+    {
+        return city1.Population > city2.Population;
+    }
+
+    
+    public static bool operator <(City city1, City city2)
+    {
+        return city1.Population < city2.Population;
+    }
+
+    //
+    public override bool Equals(object obj)
+    {
+        if (obj == null || !(obj is City))
+            return false;
+
+        City other = (City)obj;
+        return this == other;
+    }
+
+    
+    public override int GetHashCode()
+    {
+        return name.GetHashCode() ^ population.GetHashCode();
+    }
+}
 }
--- TestExercise1/UnitTest1.cs
+++ TestExercise1/UnitTest1.cs
@@ -1,11 +1,107 @@
+using CSharp4_27_02;
+
 namespace TestExercise1
 {
     [TestClass]
-    public class UnitTest1
+    public class CityTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestCityPopulationIncrease()
         {
+            
+            City city = new City("TestCity", 1000);
+
+            
+            city += 500;
+
+            
+            Assert.AreEqual(1500, city.Population);
+        }
+
+        [TestMethod]
+        public void TestCityPopulationDecrease()
+        {
+            
+            City city = new City("TestCity", 1000);
+
+            
+            city -= 300;
+
+            
+            Assert.AreEqual(700, city.Population);
+        }
+
+        [TestMethod]
+        public void TestCityPopulationDecreaseWithInvalidOperation()
+        {
+            
+            City city = new City("TestCity", 1000);
+
+            
+            Assert.ThrowsException<InvalidOperationException>(() => city -= 1500);
+        }
+
+        [TestMethod]
+        public void TestCityEquality()
+        {
+            
+            City city1 = new City("City1", 1000);
+            City city2 = new City("City2", 1000);
+            City city3 = new City("City3", 2000);
+
+            
+            Assert.IsTrue(city1 == city2);
+            Assert.IsFalse(city1 == city3);
+        }
+
+        [TestMethod]
+        public void TestCityInequality()
+        {
+            
+            City city1 = new City("City1", 1000);
+            City city2 = new City("City2", 1000);
+            City city3 = new City("City3", 2000);
+
+            
+            Assert.IsFalse(city1 != city2);
+            Assert.IsTrue(city1 != city3);
+        }
+
+        [TestMethod]
+        public void TestCityGreaterThan()
+        {
+            
+            City city1 = new City("City1", 1000);
+            City city2 = new City("City2", 2000);
+
+            
+            Assert.IsFalse(city1 > city2);
+            Assert.IsTrue(city2 > city1);
+        }
+
+        [TestMethod]
+        public void TestCityLessThan()
+        {
+            
+            City city1 = new City("City1", 1000);
+            City city2 = new City("City2", 2000);
+
+            
+            Assert.IsTrue(city1 < city2);
+            Assert.IsFalse(city2 < city1);
+        }
+
+        [TestMethod]
+        public void TestCityEqualsMethod()
+        {
+            
+            City city1 = new City("City1", 1000);
+            City city2 = new City("City2", 1000);
+            City city3 = new City("City3", 2000);
+
+            
+            Assert.IsTrue(city1.Equals(city2));
+            Assert.IsFalse(city1.Equals(city3));
         }
     }
 }
