860a9de6ea331f36d91444b8a01e4079eb2787bf
MaksimMyson <maksimmyson33@gmail.com>
2024-02-27 20:34:51 +0200

    exe4

--- CSharp4_27_02/Class1.cs
+++ CSharp4_27_02/Class1.cs
@@ -1,7 +1,91 @@
 ﻿namespace CSharp4_27_02
 {
-    public class Class1
+    public class CreditCard
     {
+        private string cardNumber;
+        private int cvc;
+        private decimal balance;
 
+        public string CardNumber
+        {
+            get { return cardNumber; }
+            set { cardNumber = value; }
+        }
+
+        public int CVC
+        {
+            get { return cvc; }
+            set { cvc = value; }
+        }
+
+        public decimal Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+
+        public CreditCard(string cardNumber, int cvc, decimal balance)
+        {
+            CardNumber = cardNumber;
+            CVC = cvc;
+            Balance = balance;
+        }
+
+        public static CreditCard operator +(CreditCard card, decimal amount)
+        {
+            card.Balance += amount;
+            return card;
+        }
+
+        public static CreditCard operator -(CreditCard card, decimal amount)
+        {
+            if (card.Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Insufficient funds.");
+            }
+
+            card.Balance -= amount;
+            return card;
+        }
+
+        public static bool operator ==(CreditCard card1, CreditCard card2)
+        {
+            if (ReferenceEquals(card1, null))
+                return ReferenceEquals(card2, null);
+            if (ReferenceEquals(card2, null))
+                return false;
+
+            return card1.CVC == card2.CVC;
+        }
+
+        public static bool operator !=(CreditCard card1, CreditCard card2)
+        {
+            return !(card1 == card2);
+        }
+
+        public static bool operator >(CreditCard card1, CreditCard card2)
+        {
+            return card1.Balance > card2.Balance;
+        }
+
+        public static bool operator <(CreditCard card1, CreditCard card2)
+        {
+            return card1.Balance < card2.Balance;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is CreditCard))
+                return false;
+
+            CreditCard other = (CreditCard)obj;
+            return this == other;
+        }
+
+        public override int GetHashCode()
+        {
+            return cardNumber.GetHashCode() ^ cvc.GetHashCode() ^ balance.GetHashCode();
+        }
     }
 }
+
--- TestExercise1/UnitTest1.cs
+++ TestExercise1/UnitTest1.cs
@@ -1,11 +1,71 @@
+using CSharp4_27_02;
+
 namespace TestExercise1
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAdditionOperator()
         {
+            CreditCard card = new CreditCard("1234567890123456", 123, 1000);
+            card += 500;
+            Assert.AreEqual(1500, card.Balance);
         }
+
+        [TestMethod]
+
+        public void TestSubtractionOperator()
+        {
+            CreditCard card = new CreditCard("1234567890123456", 123, 1000);
+            card -= 500;
+            Assert.AreEqual(500, card.Balance);
+        }
+
+        [TestMethod]
+
+        public void TestEqualityOperator()
+        {
+            CreditCard card1 = new CreditCard("1234567890123456", 123, 1000);
+            CreditCard card2 = new CreditCard("6543210987654321", 123, 2000);
+            Assert.IsTrue(card1 == card2);
+        }
+
+        [TestMethod]
+
+        public void TestInequalityOperator()
+        {
+            CreditCard card1 = new CreditCard("1234567890123456", 123, 1000);
+            CreditCard card2 = new CreditCard("6543210987654321", 456, 2000);
+            Assert.IsTrue(card1 != card2);
+        }
+
+        [TestMethod]
+
+        public void TestGreaterThanOperator()
+        {
+            CreditCard card1 = new CreditCard("1234567890123456", 123, 1000);
+            CreditCard card2 = new CreditCard("6543210987654321", 456, 2000);
+            Assert.IsTrue(card2 > card1);
+        }
+
+        [TestMethod]
+
+        public void TestLessThanOperator()
+        {
+            CreditCard card1 = new CreditCard("1234567890123456", 123, 1000);
+            CreditCard card2 = new CreditCard("6543210987654321", 456, 2000);
+            Assert.IsTrue(card1 < card2);
+        }
+
+        [TestMethod]
+
+        public void TestEqualsMethod()
+        {
+            CreditCard card1 = new CreditCard("1234567890123456", 123, 1000);
+            CreditCard card2 = new CreditCard("1234567890123456", 123, 1000);
+            Assert.IsTrue(card1.Equals(card2));
+        }
+
     }
 }
