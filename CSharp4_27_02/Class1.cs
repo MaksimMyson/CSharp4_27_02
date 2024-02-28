49b9d74ca6cf194baf2e67474b4a93781c3ce828
MaksimMyson <maksimmyson33@gmail.com>
2024-02-28 11:00:38 +0200

    exercise3

--- CSharp4_27_02/Class1.cs
+++ CSharp4_27_02/Class1.cs
@@ -1,7 +1,81 @@
 ﻿namespace CSharp4_27_02
 {
-    public class Class1
+    public class ReadingList
     {
+        private List<string> books = new List<string>();
 
+        
+        public List<string> Books
+        {
+            get { return books; }
+            set { books = value; }
+        }
+
+        
+        public void AddBook(string book)
+        {
+            books.Add(book);
+        }
+
+        
+        public void RemoveBook(string book)
+        {
+            books.Remove(book);
+        }
+
+        
+        public bool ContainsBook(string book)
+        {
+            return books.Contains(book);
+        }
+
+        
+        public string this[int index]
+        {
+            get { return books[index]; }
+            set { books[index] = value; }
+        }
+
+        
+        public static ReadingList operator +(ReadingList readingList, string book)
+        {
+            readingList.AddBook(book);
+            return readingList;
+        }
+
+        
+        public static ReadingList operator -(ReadingList readingList, string book)
+        {
+            readingList.RemoveBook(book);
+            return readingList;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadingList myReadingList = new ReadingList();
+
+            myReadingList += "Book 1";
+            myReadingList += "Book 2";
+            myReadingList += "Book 3";
+
+            Console.WriteLine("Books to Read:");
+            foreach (string book in myReadingList.Books)
+            {
+                Console.WriteLine(book);
+            }
+
+            
+            myReadingList -= "Book 2";
+
+            
+            Console.WriteLine($"Is 'Book 2' in the reading list? {myReadingList.ContainsBook("Book 2")}");
+
+            
+            Console.WriteLine($"Book at index 0: {myReadingList[0]}");
+        }
     }
 }
+
--- TestExercise1/UnitTest1.cs
+++ TestExercise1/UnitTest1.cs
@@ -1,11 +1,65 @@
+using CSharp4_27_02;
+
 namespace TestExercise1
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AddBook_AddsBookToList()
         {
+            var readingList = new ReadingList();
+
+            readingList.AddBook("Book 1");
+
+            Assert.IsTrue(readingList.ContainsBook("Book 1"));
+
+        }
+
+        [TestMethod]
+
+        public void RemoveBook_RemovesBookFromList()
+        {
+            var readingList = new ReadingList();
+
+            readingList.AddBook("Book 1");
+
+            readingList.RemoveBook("Book 1");
+
+            Assert.IsFalse(readingList.ContainsBook("Book 1"));
+        }
+
+        [TestMethod]
+
+        public void ContainsBook_ReturnsTrueIfBookExists()
+        {
+            var readingList = new ReadingList();
+
+            readingList.AddBook("Book 1");
+
+            Assert.IsTrue(readingList.ContainsBook("Book 1"));
+        }
+
+        [TestMethod]
+
+        public void ContainsBook_ReturnsFalseIfBookDoesNotExist()
+        {
+            var readingList = new ReadingList();
+
+            readingList.AddBook("Book 1");
+
+            Assert.IsFalse(readingList.ContainsBook("Book 2"));
+        }
+
+        [TestMethod]
+
+        public void Indexer_CanAccessBookByIndex()
+        {
+            var readingList = new ReadingList();
+
+            readingList.AddBook("Book 1");
+
+            Assert.AreEqual("Book 1", readingList[0]);
         }
     }
 }
