5e5b9822ef59084cc1151d496ad8eebac0e42647
MaksimMyson <maksimmyson33@gmail.com>
2024-02-28 10:30:42 +0200

    exercise2

--- CSharp4_27_02/Class1.cs
+++ CSharp4_27_02/Class1.cs
@@ -1,7 +1,81 @@
 ﻿namespace CSharp4_27_02
 {
-    public class Class1
+    public class Store
     {
+        private string name;
+        private double area;
 
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Area
+        {
+            get { return area; }
+            set { area = value; }
+        }
+
+        public Store(string name, double area)
+        {
+            Name = name;
+            Area = area;
+        }
+
+        public static Store operator +(Store store, double increase)
+        {
+            store.Area += increase;
+            return store;
+        }
+
+        public static Store operator -(Store store, double decrease)
+        {
+            if (store.Area - decrease < 0)
+                throw new InvalidOperationException("Площа магазину не може стати від'ємною.");
+
+            store.Area -= decrease;
+            return store;
+        }
+
+        public static bool operator ==(Store store1, Store store2)
+        {
+            if (ReferenceEquals(store1, null))
+                return ReferenceEquals(store2, null);
+            if (ReferenceEquals(store2, null))
+                return false;
+
+            return store1.Area == store2.Area;
+        }
+
+        public static bool operator !=(Store store1, Store store2)
+        {
+            return !(store1 == store2);
+        }
+
+        public static bool operator >(Store store1, Store store2)
+        {
+            return store1.Area > store2.Area;
+        }
+
+        public static bool operator <(Store store1, Store store2)
+        {
+            return store1.Area < store2.Area;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Store))
+                return false;
+
+            Store other = (Store)obj;
+            return this == other;
+        }
+
+        public override int GetHashCode()
+        {
+            return name.GetHashCode() ^ area.GetHashCode();
+        }
     }
 }
+
--- TestExercise1/UnitTest1.cs
+++ TestExercise1/UnitTest1.cs
@@ -1,11 +1,59 @@
+using CSharp4_27_02;
+
 namespace TestExercise1
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestStoreAddition()
         {
+            
+            Store store = new Store("Test Store", 100.0);
+
+            
+            store += 50.0;
+
+            
+            Assert.AreEqual(150.0, store.Area);
+        }
+
+        [TestMethod]
+
+        public void TestStoreSubtraction()
+        {
+            
+            Store store = new Store("Test Store", 100.0);
+
+            
+            store -= 50.0;
+
+            
+            Assert.AreEqual(50.0, store.Area);
+        }
+
+        [TestMethod]
+
+        public void TestStoreEquality()
+        {
+            
+            Store store1 = new Store("Test Store 1", 100.0);
+            Store store2 = new Store("Test Store 2", 100.0);
+
+            
+            Assert.IsTrue(store1 == store2);
+        }
+
+        [TestMethod]
+
+        public void TestStoreComparison()
+        {
+            
+            Store store1 = new Store("Test Store 1", 100.0);
+            Store store2 = new Store("Test Store 2", 50.0);
+
+            
+            Assert.IsTrue(store1 > store2);
         }
     }
 }
