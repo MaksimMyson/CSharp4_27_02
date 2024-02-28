95adee93f97d18921901d20a3c2971fa33eaa350
MaksimMyson <maksimmyson33@gmail.com>
2024-02-28 10:21:45 +0200

    exercise1

--- CSharp4_27_02/Class1.cs
+++ CSharp4_27_02/Class1.cs
@@ -1,7 +1,80 @@
 ﻿namespace CSharp4_27_02
 {
-    public class Class1
+    public class Journal
     {
+        private string name;
+        private int numberOfEmployees;
 
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int NumberOfEmployees
+        {
+            get { return numberOfEmployees; }
+            set { numberOfEmployees = value; }
+        }
+
+        public Journal(string name, int numberOfEmployees)
+        {
+            Name = name;
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public static Journal operator +(Journal journal, int increase)
+        {
+            journal.NumberOfEmployees += increase;
+            return journal;
+        }
+
+        public static Journal operator -(Journal journal, int decrease)
+        {
+            if (journal.NumberOfEmployees - decrease < 0)
+                throw new InvalidOperationException("Неможливо відняти від кількості працівників, що вже є нульовою або від'ємною.");
+
+            journal.NumberOfEmployees -= decrease;
+            return journal;
+        }
+
+        public static bool operator ==(Journal journal1, Journal journal2)
+        {
+            if (ReferenceEquals(journal1, null))
+                return ReferenceEquals(journal2, null);
+            if (ReferenceEquals(journal2, null))
+                return false;
+
+            return journal1.NumberOfEmployees == journal2.NumberOfEmployees;
+        }
+
+        public static bool operator !=(Journal journal1, Journal journal2)
+        {
+            return !(journal1 == journal2);
+        }
+
+        public static bool operator >(Journal journal1, Journal journal2)
+        {
+            return journal1.NumberOfEmployees > journal2.NumberOfEmployees;
+        }
+
+        public static bool operator <(Journal journal1, Journal journal2)
+        {
+            return journal1.NumberOfEmployees < journal2.NumberOfEmployees;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || !(obj is Journal))
+                return false;
+
+            Journal other = (Journal)obj;
+            return this == other;
+        }
+
+        public override int GetHashCode()
+        {
+            return name.GetHashCode() ^ numberOfEmployees.GetHashCode();
+        }
     }
 }
--- TestExercise1/UnitTest1.cs
+++ TestExercise1/UnitTest1.cs
@@ -1,11 +1,64 @@
+using CSharp4_27_02;
 namespace TestExercise1
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestJournalAddition()
         {
+            Journal journal = new Journal("Test Journal", 5);
+
+            
+            journal += 3;
+
+           
+            Assert.AreEqual(8, journal.NumberOfEmployees);
         }
-    }
+
+        [TestMethod]
+
+        public void TestJournalSubtraction()
+        {
+            Journal journal = new Journal("Test Journal", 5);
+
+            
+            journal -= 3;
+
+            
+            Assert.AreEqual(2, journal.NumberOfEmployees);
+        }
+
+        [TestMethod]
+
+        public void TestJournalEquality()
+        {
+            Journal journal1 = new Journal("Test Journal 1", 5);
+            Journal journal2 = new Journal("Test Journal 2", 5);
+
+            
+            Assert.IsTrue(journal1 == journal2);
+        }
+
+        [TestMethod]
+
+        public void TestJournalInequality()
+        {
+            Journal journal1 = new Journal("Test Journal 1", 5);
+            Journal journal2 = new Journal("Test Journal 2", 3);
+
+            
+            Assert.IsTrue(journal1 != journal2);
+        }
+
+        [TestMethod]
+
+        public void TestJournalComparison()
+        {
+            Journal journal1 = new Journal("Test Journal 1", 5);
+            Journal journal2 = new Journal("Test Journal 2", 3);
+
+
+            Assert.IsTrue(journal1 > journal2);
+    }   }
 }
